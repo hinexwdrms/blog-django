104b0448978eac5281772b1eef0cbadb93661117
sarbesh23549 <sarbesh23549@gmail.com>
2026-05-12 22:36:20 +0545

    posts views

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello")
+    posts = Post.objects.all()
+    return HttpResponse(f"Total posts: {posts.count()}")
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return HttpResponse(f"Post: {post.title}")
