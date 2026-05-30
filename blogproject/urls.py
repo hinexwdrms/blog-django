c0d582bbddbd1d887e3a958d0b493c462868621e
sarbesh23549 <sarbesh23549@gmail.com>
2026-05-30 17:59:06 +0545

    user register

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":                
+        form = UserCreationForm(request.POST)   #fills the form
+        if form.is_valid():                     #checks if everything submitted correctly
+            form.save()                         #creates user in database
+            return redirect('home')             
+    else:                                       #if GET request
+        form = UserCreationForm()               
+    return render(request, 'users/register.html', {'form': form})
