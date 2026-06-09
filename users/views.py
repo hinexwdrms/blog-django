9fb7de9d0b77d146a642fd6bf541706cb023836b
sarbesh23549 <sarbesh23549@gmail.com>
2026-06-09 20:56:11 +0545

    user register/login/logout + navbar

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -12,3 +13,17 @@ def register(request):
     else:                                       #if GET request
         form = UserCreationForm()               
     return render(request, 'users/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    return render(request, 'users/login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
